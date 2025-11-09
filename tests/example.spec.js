9088c0720a858581f2729de32f01c10a8cfd91ee
AndreaSnajdr <andrea.pavlasova@gmail.com>
2025-11-09 16:07:38 +0100

    firts test added

---
+++ tests/example.spec.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+//test('has title', async ({ page }) => {
+//await page.goto('https://playwright.dev/');
+
+// Expect a title "to contain" a substring.
+//await expect(page).toHaveTitle(/Playwright/);
+//});
+
+test('navigates to Czechitas', async ({page}) => {
+ await page.goto('/prihlaseni');
+
+ await page.getByRole('textbox', { name: 'Email' }).fill("alena.patek@email.cz");
+ await page.getByRole('textbox', { name: 'Heslo' }).fill("password");
+
+ await page.getByRole('button', { name: 'Přihlásit' }).click();
+
+ await expect(page.locator(".invalid-feedback > strong:nth-child(1)")).toBeVisible();
+
+  // Click the get started link.
+  //await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
